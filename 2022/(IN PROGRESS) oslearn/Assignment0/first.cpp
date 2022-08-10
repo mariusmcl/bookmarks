b66cfb30b996b0e582e4ffbe4805dcb157265118
Marius Landsverk <marius_cl@hotmail.com>
2022-08-10 11:58:00 +0200

    os, d2lrecommender + some probml ch3

---
+++ 2022/(IN PROGRESS) oslearn/Assignment0/first.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // int pid = fork()
+    int pid = fork()
+    
+    cout << "123" << endl;
+
+    /* for (const string& word : msg)
+    {
+        cout << word << " ";
+    } */
+    cout << endl;
+}
---
+++ 2022/(IN PROGRESS) probml book2/ch3.md
@@ -0,0 +1,5 @@
+Frequentist vs bayesian: frequentist methods use sampling distributions to represent/ gauge uncertainty. 
+Bayesians benytter (priors) regner jeg med? 
+
+'It is the use of a sampling distribution to represent uncertainty that makes a method frequentist. If we ignore uncertainty, we are just performing an optimization problem, which is neither frequentist or Bayesian'.
+
